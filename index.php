b17831fed3aab8383943532b28f067272644ed80
bénéwindé <kontieboange@gmail.com>
2025-01-19 18:55:33 +0100

    creation de la page d'acceuil en html avec un logo une barre de navigation et un footer avec les differentes informations sur l'entreprise de location

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vroomly</title>
+    <link rel="stylesheet" href="acceuil_footer.css">
+    <link rel="stylesheet" href="acceuil.css">
+</head>
+<body>
+    <div class="conteneur">
+        <div class="contenu">
+            <div class="container">
+                <nav>
+                <div class="logo-container">
+                <img src="logo.png" alt="" class="logo" />
+                </div>
+
+                        <ul>
+                            <li><a href="#">Accueil</a></li>
+                            <li><a href="vehicules.php">voitures</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+
+                </nav>
+
+                <section class="site-container">
+                    <p>Bienvenue sur</p>
+                    <h1>VROOMLY</h1>
+                    <h3>Locations de voitures</h3>
+
+                </section>
+
+                </section>
+            </div>
+        </div>
+
+        <footer>
+            <div class="contenu-footer">
+                <div class="footer-services bloc">
+                    <h3>Nos services</h3>
+                    <p>Location de voitures</p>
+                </div>
+
+                <div class="footer-contact bloc">
+                    <h3>Nous contacter</h3>
+                    <p>Téléphone : 55-55-55-55</p>
+                    <p>Email : vroomly@gmail.com</p>
+                    <p>Adresse : 6 rue de l'invention, Le Mans, 72100</p>
+                </div>
+
+                <div class="footer-horaires bloc">
+                    <h3>Nos horaires</h3>
+                    <p>Ouvert 7j/7</p>
+                    <p>24h/24</p>
+                </div>
+            </div>
+
+            <div class="pied-de-page">
+                <p>&copy; 2025 Vroomly. Tous droits réservés.</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
